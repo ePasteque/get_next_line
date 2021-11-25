46358fe530e9b6eba3bb5dc8c82403e2cfc223b0
Loup Battesti <lbattest@z1r5p4.42lyon.fr>
2021-11-25 17:28:32 +0100

    4 save

---
+++ get_next_line.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbattest <lbattest@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 11:59:47 by lbattest          #+#    #+#             */
+/*   Updated: 2021/11/25 17:28:05 by lbattest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*extract_nl(char **str)
+{
+	char	*save;
+	char	*tmp;
+
+	tmp = ft_strndup(*str, '\n');
+	save = ft_strndup(ft_strchr(*str, '\n') + 1, '\0');
+	free(*str);
+	*str = save;
+	return (tmp);
+}
+
+static char	*scotch(char *str)
+{
+	free(str);
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	int			i;
+	char		buf[BUFFER_SIZE + 1];
+	static char	*str = NULL;
+	char		*tmp;
+
+	tmp = 0;
+	i = 1;
+	if (read(fd, buf, 0) < 0)
+		return (NULL);
+	while (!ft_strchr(str, '\n') && i != 0)
+	{
+		i = read(fd, buf, BUFFER_SIZE);
+		if (i < 0)
+			return (0);
+		buf[i] = '\0';
+		str = ft_strjoin(str, buf);
+		if (!str)
+			return (0);
+	}
+	if (ft_strchr(str, '\n'))
+		return (extract_nl(&str));
+	if (str && str[0])
+		tmp = ft_strndup(str, '\0');
+	str = scotch(str);
+	return (tmp);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbattest <lbattest@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 12:00:37 by lbattest          #+#    #+#             */
+/*   Updated: 2021/11/24 19:23:44 by lbattest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strndup(char *s1, char c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
